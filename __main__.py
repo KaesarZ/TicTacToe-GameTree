80100e53b39578f6ffdf4992d0784b301505b49d
KaesarZ <jccb2@cin.ufpe.br>
2019-06-18 08:11:56 -0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
+'''
+    Project: TicTacToe - Game Tree
+    Description: Implementation of a Heap Structure and Heap Sort
+    Authors: Julio Cesar de Carvalho Barros (jccb2@cin.ufpe.br)
+             Guilherme Guerra Campos (ggc3@cin.ufpe.br)
+'''
 from TicTacToe import *
 
 if __name__ == '__main__':
